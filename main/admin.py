8237a449a172e7ffba5dd7276ec65f297d1866f3
mub1nakhan <arkadashler82@gmail.com>
2026-02-17 13:52:41 +0500

    main_py

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
-
+from .models import Student,Course,Profile
 # Register your models here.
+
+class ProfileInlineAdmin(admin.TabularInline):
+    model = Profile
+    max_num = 1
+    can_delete = False
+
+class StudentAdmin(admin.ModelAdmin):
+    inlines = [ProfileInlineAdmin]
+    list_display = ["id","full_name","phone_number","created_at","updated_at"]
+    list_display_links =["full_name"]
+    list_filter = ["created_at","updated_at"]
+    search_fields = ["first_name","last_name","phone_number"]
+    fields = ["first_name","last_name","phone_number","courses","created_at","updated_at"]
+    readonly_fields = ["created_at", "updated_at","id"]
+
+
+class CourseAdmin(admin.ModelAdmin):
+    list_display =["id","title","price","discount","final_price"]
+    search_fields = ["title","description"]
+    list_filter = ["discount_type"]
+    ordering = ["-price"]
+
+
+admin.site.register(Student, StudentAdmin)
+admin.site.register(Course, CourseAdmin)
--- main/models.py
+++ main/models.py
@@ -1,3 +1,55 @@
 from django.db import models
 
+
 # Create your models here.
+    
+    
+class Course(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(null=True,blank=True)
+    price = models.IntegerField(default=0)
+    
+    discount_type_choices =(
+        ("percent", "foizli"),
+        ("flex", "aniq")
+    )
+    
+    discount_type = models.CharField(max_length=25, choices=discount_type_choices)
+    
+    discount = models.IntegerField(default=0)
+    
+    def final_price(self):
+        if self.discount:
+            if self.discount_type == "percent":
+                
+                return self.price - (self.price / 100 * self.discount)
+            return self.price - self.discount
+        return self.price
+    
+    
+    
+    def __str__(self):
+        return self.title
+    
+    
+class Student(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255, blank=True, null=True)
+    phone_number = models.CharField(max_length=15, null=True, blank=True)
+    
+    courses =models.ManyToManyField(Course)
+    
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def full_name(self):
+        return self.first_name + " " + str(self.last_name) if self.last_name else self.first_name
+    
+    
+    
+class Profile(models.Model):
+    student = models.OneToOneField(Student,on_delete=models.CASCADE)
+    address = models.CharField(max_length=255,null=True,blank=True)
+    
+    
+    
